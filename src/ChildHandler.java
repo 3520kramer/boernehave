b5b2276ea8c558de1f849ba2760a47628adcb22e
Oliver Kramer <3520_kramer@live.dk>
2019-03-20 22:44:31 +0100

    program done

--- src/ChildHandler.java
+++ src/ChildHandler.java
@@ -48,27 +48,120 @@ public class ChildHandler {
     }
 
     public void addToFile() throws FileNotFoundException{
-        PrintStream output = new PrintStream(new File("memberList.txt"));
+        PrintStream output = new PrintStream(new File("src/memberFile.txt"));
         for (Child ch : childList){
-            output.print(ch.getFirstName()+"-"+ch.getLastName()+"-"+ch.getBirthDate()+"-"+ch.getStreetName() +"-"+ch.getStreetNumber()+"-"
-                    +ch.getPostalCode()+"-"+ch.getCity()+"-"+ch.getContactPerson1()+"-"+ch.getContactPerson2());
+            output.print(ch.getFirstName() + "-" + ch.getLastName() + "-" + ch.getBirthDate() + "-" + ch.getStreetName() + "-" + ch.getStreetNumber() + "-"
+                    + ch.getPostalCode() + "-" + ch.getCity() + "-" + ch.getContactPerson1() + "-" + ch.getContactPerson2() + "-");
         }
     }
 
-    public void printTest() {
-        System.out.println("Børn: ");
-
+    public void displayList() {
         for(Child ch : childList) {
             System.out.println("Fornavn: " + ch.getFirstName());
             System.out.println("Efternavn: " + ch.getLastName());
             System.out.println("Fødselsdato: "+ ch.getBirthDate());
-            System.out.println("Adresse\n Gadenavn:"+ch.getStreetName());
+            System.out.println("Adresse\nGadenavn:"+ch.getStreetName());
             System.out.println("Nummer: "+ch.getStreetNumber());
             System.out.println("Postnummer: "+ch.getPostalCode());
             System.out.println("By: "+ch.getCity());
             System.out.println("Kontakt Person 1: "+ch.getContactPerson1());
-            System.out.println("Kontakt Person 2: "+ch.getContactPerson2());
+            System.out.println("Kontakt Person 2: "+ch.getContactPerson2() + "\n\n");
 
+        }
+    }
+
+    public void searchToEdit() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hvem vil du søge efter?");
+        String input = scan.next();
+
+        Child child = new Child();
+        int count = 0;
+        int pos = 0;
+
+        for (Child ch : childList) {
+            if (input.equalsIgnoreCase(ch.getFirstName()) || input.equalsIgnoreCase(ch.getLastName())
+                    || input.equalsIgnoreCase(ch.getBirthDate())) {
+                child = ch;
+                count++;
+                childList.remove(pos);
+            }
+            pos++;
+        }
+        System.out.println("Tryk 1 for at ændre fornavn:\nTryk 2 for at ændre efternavn\ntryk 3 for at ændre fødselsdato\n" +
+                "Tryk 4 for at ændre adresse\nTryk 5 for at ændre kontaktperson 1\nTryk 6 for at ændre kontaktperson 2\n");
+
+        int numInput = scan.nextInt();
+        switch (numInput){
+            case 1:
+                input = scan.next();
+                child.setFirstName(input);
+                break;
+            case 2:
+                input = scan.next();
+                child.setLastName(input);
+                break;
+            case 3:
+                input = scan.next();
+                child.setBirthDate(input);
+                break;
+            case 4:
+                input = scan.next();
+                child.setStreetName(input);
+
+                numInput = scan.nextInt();
+                child.setStreetNumber(numInput);
+
+                input = scan.next();
+                child.setCity(input);
+
+                numInput = scan.nextInt();
+                child.setPostalCode(numInput);
+                break;
+            case 5:
+                numInput = scan.nextInt();
+                child.setContactPerson1(numInput);
+                break;
+            case 6:
+                numInput = scan.nextInt();
+                child.setContactPerson2(numInput);
+                break;
+        }
+        childList.add(child);
+    }
+
+    public void search(){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hvem vil du søge efter?");
+        String input = scan.next();
+
+        int count = 0;
+        for(Child ch : childList) {
+            if (input.equalsIgnoreCase(ch.getFirstName()) || input.equalsIgnoreCase(ch.getLastName())
+                    || input.equalsIgnoreCase(ch.getBirthDate())) {
+                count++;
+                System.out.println(count + " resultater fundet");
+            }
+        }
+
+        count = 0;
+        for(Child ch : childList){
+            if(input.equalsIgnoreCase(ch.getFirstName()) || input.equalsIgnoreCase(ch.getLastName())
+                    || input.equalsIgnoreCase(ch.getBirthDate())){
+                count++;
+                System.out.println("Resultat nr." + count + ":");
+                System.out.println("Fornavn: " + ch.getFirstName());
+                System.out.println("Efternavn: " + ch.getLastName());
+                System.out.println("Fødselsdato: "+ ch.getBirthDate());
+                System.out.println("Adresse\nGadenavn: "+ch.getStreetName());
+                System.out.println("Nummer: "+ch.getStreetNumber());
+                System.out.println("Postnummer: "+ch.getPostalCode());
+                System.out.println("By: "+ch.getCity());
+                System.out.println("Kontakt Person 1: "+ch.getContactPerson1());
+                System.out.println("Kontakt Person 2: "+ch.getContactPerson2());
+            }
         }
     }
 
@@ -78,19 +171,19 @@ public class ChildHandler {
         Child child = new Child();
 
         System.out.println("Indtast fornavn: ");
-        String text = input.nextLine();
+        String text = input.next();
         child.setFirstName(text);
 
         System.out.println("Indtast efternavn:");
-        text = input.nextLine();
+        text = input.next();
         child.setLastName(text);
 
         System.out.println("Indtast fødselsdagsdato: ");
-        text = input.nextLine();
+        text = input.next();
         child.setBirthDate(text);
 
         System.out.println("Indtast vejnavn: ");
-        text = input.nextLine();
+        text = input.next();
         child.setStreetName(text);
 
         System.out.println("Indtast husnummer: ");
@@ -102,7 +195,7 @@ public class ChildHandler {
         child.setPostalCode(num);
 
         System.out.println("Indtast by: ");
-        text = input.nextLine();
+        text = input.next();
         child.setCity(text);
 
         System.out.println("Indtast kontaktperson nr. 1s telefonnummer: ");
@@ -112,5 +205,7 @@ public class ChildHandler {
         System.out.println("Indtast kontaktperson nr. 2s telefonnummer: ");
         num = input.nextInt();
         child.setContactPerson2(num);
+
+        childList.add(child);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception{
 
         Child child = new Child();
         ChildHandler handler = new ChildHandler();
+        Scanner scan = new Scanner(System.in);
 
         handler.addFromFile();
-        handler.printTest();
-        handler.addToFile();
+        while(true) {
+            System.out.println("Roskilde frie børnehaves program");
+            System.out.println("Tryk 1 for at registrere et nyt barn\nTryk 2 for at se alle børn eller søge\ntryk 3 for at redigere et barn\n" +
+                    "Tryk 4 for at registrere personale\nTryk 5 for at se personale\nTryk 6 for at redigere personale\nTryk 7 for at se venteliste\n" +
+                    "Tryk 8 for at redigere venteliste\nTryk 9 for at se vagtplan\nTryk 10 for at redigere vagtplan\nTryk 11 for telefonliste\nTryk 12 for exit");
+            int input = scan.nextInt();
+            if (input == 1) {
+                handler.registerNewChild();
+                handler.addToFile();
+            }
+            if (input == 2) {
+                System.out.println("Tast 1 for at se alle børn\nTast 2 for at søge");
+                input = scan.nextInt();
+                if (input == 1) {
+                    handler.displayList();
+                }
+                if (input == 2) {
+                    handler.search();
+                }
+            }
+            if (input == 3) {
+                handler.searchToEdit();
+                handler.addToFile();
+            }
+            if (input >= 4 && input <= 11) {
+                System.out.println("...");
+            }
+            if(input == 12) {
+                break;
+            }
+            if(input > 12){
+                System.out.println("Tast korrekt, tak! \n");
+            }
+        }
     }
-
 }
